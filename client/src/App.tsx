20a172c566a5e46fdbef1c4294e6aeb994100325
Rivky <r056757343@gmail.com>
2026-01-13 18:06:25 +0200

    Add client React project

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ padding: '40px', fontSize: '24px' }}>
+      <h1>RecipeBox Client Works ✅</h1>
+    </div>
+  );
+}
+
+export default App;
