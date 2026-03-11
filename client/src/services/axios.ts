ea25ebfdd2c3bd9dbd54085d5010d8e90a66fa1e
Rivky <r056757343@gmail.com>
2026-03-11 15:55:13 +0200

    add env to client

--- client/src/services/axios.ts
+++ client/src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:6001',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:6001',
 });
 
 // Attach JWT token from localStorage to every request
